cf393c0d1cbb7fe1e4bc45dd6bf14ea56e4eba65
mattiapomelli <mattiapomelli@gmail.com>
2023-01-09 14:58:31 +0000

    Send gasless erc20 transfer

--- dapp/src/pages/index.tsx
+++ dapp/src/pages/index.tsx
@@ -1,11 +1,10 @@
 import styles from "../styles/Home.module.css";
 import { useEffect, useState } from "react";
 import { ConnectButton } from "@rainbow-me/rainbowkit";
-import { BigNumber, ethers, providers } from "ethers";
+import { ethers, providers } from "ethers";
 import { ChainId, SmartAccountConfig } from "@biconomy/core-types";
 import SmartAccount from "@biconomy/smart-account";
 import { useAccount, useProvider, useSigner } from "wagmi";
-import { useLockContract } from "../hooks/use-contract";
 
 const options: Partial<SmartAccountConfig> = {
   activeNetworkId: ChainId.GOERLI,
@@ -19,14 +18,8 @@ const options: Partial<SmartAccountConfig> = {
 };
 
 const Home = () => {
-  const provider = useProvider();
   const { data: signer } = useSigner();
   const { address } = useAccount();
-
-  const lockContract = useLockContract();
-  const [contractBalance, setContractBalance] = useState<BigNumber>(
-    BigNumber.from(0)
-  );
 
   const [smartAccount, setSmartAccount] = useState<SmartAccount | null>(null);
 
@@ -38,8 +31,8 @@ const Home = () => {
         (signer?.provider as providers.Web3Provider).provider
       );
 
-      let smartAccount = new SmartAccount(walletProvider, options);
-      smartAccount = await smartAccount.init();
+      const smartAccount = new SmartAccount(walletProvider, options);
+      await smartAccount.init();
 
       setSmartAccount(smartAccount);
     };
@@ -47,46 +40,56 @@ const Home = () => {
     setupSmartAccount();
   }, [address, signer?.provider]);
 
-  useEffect(() => {
-    if (!lockContract) return;
+  const onDeposit = async () => {
+    if (!smartAccount) return;
 
-    const getLockBalance = async () => {
-      const balance = await provider.getBalance(lockContract.address);
-      setContractBalance(balance);
+    const tokenAddress = "0xC2C527C0CACF457746Bd31B2a698Fe89de2b6d49";
+    const recipientAddress = "0x498c3DdbEe3528FB6f785AC150C9aDb88C7d372c";
+
+    // One needs to prepare the transaction data
+    // Here we will be transferring ERC 20 tokens from the Smart Contract Wallet to an address
+    const erc20Interface = new ethers.utils.Interface([
+      "function transfer(address _to, uint256 _value)",
+    ]);
+
+    // Encode an ERC-20 token transfer to recipientAddress of the specified amount
+    const encodedData = erc20Interface.encodeFunctionData("transfer", [
+      recipientAddress,
+      1 * 10 ** 6,
+    ]);
+
+    // You need to create transaction objects of the following interface
+    const tx = {
+      to: tokenAddress, // destination smart contract address
+      data: encodedData,
     };
 
-    getLockBalance();
-  }, [lockContract, provider]);
-
-  const onDeposit = async () => {
-    if (!lockContract || !smartAccount) return;
-
+    // Optional: Transaction subscription. One can subscribe to various transaction states
+    // Event listener that gets triggered once a hash is generetaed
     smartAccount.on("txHashGenerated", (response: any) => {
       console.log(">>> txHashGenerated event received via emitter", response);
     });
-
+    smartAccount.on("onHashChanged", (response: any) => {
+      console.log(">>> onHashChanged event received via emitter", response);
+    });
+    // Event listener that gets triggered once a transaction is mined
     smartAccount.on("txMined", (response: any) => {
       console.log(">>> txMined event received via emitter", response);
     });
-
+    // Event listener that gets triggered on any error
     smartAccount.on("error", (response: any) => {
       console.log(">>> error event received via emitter", response);
     });
 
-    const tx = await lockContract.populateTransaction.deposit({
-      value: ethers.utils.parseEther("0.0000000000000001"),
+    // Sending gasless transaction
+    const txResponse = await smartAccount.sendGaslessTransaction({
+      transaction: tx,
     });
-    const { data, value, to } = tx;
+    console.log(">>> tx hash generated", txResponse.hash);
 
-    const txResponse = await smartAccount.sendGasLessTransaction({
-      transaction: {
-        to: to || "",
-        data,
-        value,
-      },
-    });
-
-    console.log("Transaction hash", txResponse.hash);
+    // If you do not subscribe to listener, one can also get the receipt like shown below
+    const receipt = await txResponse.wait();
+    console.log(">>> tx receipt", receipt);
   };
 
   return (
@@ -102,10 +105,9 @@ const Home = () => {
           </div>
         )}
         <div>
-          <button onClick={onDeposit}>Deposit</button>
-        </div>
-        <div>
-          <p>Contract Balance: {contractBalance.toString()}</p>
+          <button className={styles.button} onClick={onDeposit}>
+            Call
+          </button>
         </div>
       </main>
     </div>
